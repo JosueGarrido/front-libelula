3cae2a62933fcf86b22a443cd89f81627d597425
josuegarrido <wocking589@gmail.com>
2024-09-20 11:54:46 -0500

    fix build

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 export default function Navbar() {
     return (
       <nav className="bg-gray-800 text-white shadow-md fixed w-full z-10 top-0 left-0">
         <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-          <a href="/" className="text-2xl font-bold">LibelulaStore</a>
+          <Link href="/" className="text-2xl font-bold">LibelulaStore</Link>
           <div className="space-x-6">
-            <a href="/" className="hover:text-gray-300">Inicio</a>
-            <a href="/" className="hover:text-gray-300">Carrito</a>
+            <Link href="/" className="hover:text-gray-300">Inicio</Link>
+            <Link href="/" className="hover:text-gray-300">Carrito</Link>
           </div>
         </div>
       </nav>
